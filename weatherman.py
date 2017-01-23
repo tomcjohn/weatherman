3c0261557f6d808b55a7779b74f4bcadac185d9b
Tom John <tom.john@rea-group.com>
2017-01-23 16:28:56 +1100

    Add simple handling for missing temp/forecast values

--- weatherman.py
+++ weatherman.py
@@ -2,6 +2,7 @@ from lxml import etree
 from ftplib import FTP
 import StringIO
 import boto3
+import time
 
 
 print('Loading function')
@@ -28,13 +29,13 @@ def get_precis_forecast():
     
     xpath_expr = '//area[@aac="VIC_PT042"]/forecast-period[@index="0"]/element[@type="air_temperature_maximum"]'
     nodes = tree.xpath(xpath_expr)
-    max_temp = nodes[0].text
+    max_temp = nodes[0].text if nodes and len(nodes) > 0 else None
     
     xpath_expr = '//area[@aac="VIC_PT042"]/forecast-period[@index="0"]/text[@type="precis"]'
     nodes = tree.xpath(xpath_expr)
-    short_forecast = nodes[0].text
+    short_forecast = nodes[0].text if nodes and len(nodes) > 0 else None
     
-    return max_temp + " - " + short_forecast
+    return max_temp + " - " + short_forecast if max_temp else short_forecast
 
 
 def get_city_forecast():
@@ -69,6 +70,7 @@ def lambda_handler(event, context):
     print precis_forecast
     print detailed_forecast
     send_as_sms(precis_forecast)
+    time.sleep(1)
     send_as_sms(detailed_forecast)
 
     return
